c666b81533f848cb9f1c4916764c5c19355c1fc7
Dan Heslinga <drdan@heslingamd.com>
2021-07-10 16:24:12 -1000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,51 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    /*
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-          // title: 'Flutter Demo Home Page',
-          ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {*/
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              'Flutter McFlutter',
-              style: Theme.of(context).textTheme.headline5,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.account_circle, size: 50),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Flutter McFlutter',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    Text('Experienced App Developer'),
+                  ],
+                ),
+              ],
             ),
-            Text('Experienced App Developer'),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text('123 Main Street'), Text('(415) 555-0198')],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.accessibility),
+                Icon(Icons.timer),
+                Icon(Icons.phone_android),
+                Icon(Icons.phone_iphone),
+              ],
+            ),
           ],
         ),
       ),
